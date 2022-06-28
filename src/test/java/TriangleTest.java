df5f8971fcce91b22614dd4b5693bee683c7eaa6
Dmitry Pestunov <PestynOFF@gmail.com>
2022-06-28 14:12:23 +0200

    Complete task 12

---
+++ src/test/java/TriangleTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TriangleTest {
+    private static Triangle t = new Triangle();
+    private static Logger logger = LoggerFactory.getLogger(TriangleTest.class);
+
+    @Test
+    public void testJudge() { // Unable to form a triangle
+        assertEquals(false, t.judge(2, 1, 3));
+    }
+    @Test
+    public void testJudge1() { // Can form a triangle
+        assertEquals(true, t.judge(3, 4, 5));
+    }
+    @Test
+    public void testJudge2() { //Cannot form a negative triangle
+        assertEquals(false, t.judge(-3, -4, -5));
+    }
+
+}
